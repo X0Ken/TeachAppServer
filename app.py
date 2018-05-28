1811dc79644a06eee9fcf96d16702b051c80fe4f
wanghongxu <wang19930902@gmail.com>
2018-05-29 00:06:34 +0800

    add job

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Column, BigInteger, String
+from datetime import datetime
+import json
+import uuid
+
+from sqlalchemy import BigInteger, Column, DateTime, String, Integer
 from tornado.gen import coroutine
 from tornado.ioloop import IOLoop
-from tornado.options import options, define
-from tornado_sqlalchemy import (as_future, declarative_base,
-                                make_session_factory, SessionMixin)
-from tornado.web import RequestHandler, Application
+from tornado.options import define, options
+from tornado.web import Application, RequestHandler
+from tornado_sqlalchemy import (SessionMixin, as_future, declarative_base,
+                                make_session_factory)
+
 
 define("database_url", 
     default="mysql+pymysql://root:aaaaaa@localhost/my_db?charset=utf8", 
@@ -13,15 +18,43 @@ define("database_url",
 
 DeclarativeBase = declarative_base()
 
+AVALIBLE_ROLE = ['teach', 'student', 'administrator']
+AVALIBLE_METHOD = ['remote', 'home']
+AVALIBLE_GENDER = ['male ', 'female']
+AVALIBLE_EDUCATION = ['Undergraduate', 'master', 'PhD']
+
 
 class User(DeclarativeBase):
     __tablename__ = 'users'
 
-    id = Column(BigInteger, primary_key=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True)
     password = Column(String(255))
-    type = Column(String(255))
+    role = Column(String(255))
+    
 
+class Job(DeclarativeBase):
+    __tablename__ = 'jobs'
+
+    uuid = Column(String(32), primary_key=True)
+    # 授课方式
+    method = Column(String(255))
+    gender = Column(String(255))
+    school = Column(String(255))
+    highest_education = Column(String(255))
+    pay = Column(String(255))
+    region = Column(String(255))
+    subject = Column(String(255))
+    time = Column(String(255))
+
+    deleted = Column(BigInteger, default=0)
+    hidded = Column(BigInteger, default=0)
+    create_at = Column(DateTime, default=datetime.now)
+    update_at = Column(DateTime, nullable=True)
+    delete_at = Column(DateTime, nullable=True)
+
+
+    
 
 class IndexHandler(RequestHandler, SessionMixin):
     @coroutine
@@ -118,6 +151,57 @@ class UserHandler(RequestHandler, SessionMixin):
             {}
             """.format(username, login))
 
+class JobHandler(RequestHandler, SessionMixin):
+    def get_job(self, job):
+        return {
+                "uuid": job.uuid,
+                "method": job.method,
+                "gender": job.gender,
+                "school": job.school,
+                "highest_education": job.highest_education,
+                "pay": job.pay,
+                "region": job.region,
+                "subject": job.subject,
+                "time": job.time,
+                "create_at": job.create_at.strftime('%Y-%m-%d %H:%M:%S')  
+                }
+
+    @coroutine
+    def get(self):
+        with self.make_session() as session:
+            jobs = session.query(Job).filter_by(deleted = 0).all()
+            jobs = (self.get_job(job) for job in jobs)
+            self.set_header("Access-Control-Allow-Origin", "*")
+            self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+            self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+            self.write({
+                "jobs": [job for job in jobs]
+                })
+    
+    @coroutine
+    def post(self):
+        job_uuid = uuid.uuid4().hex
+        body = json.loads(self.request.body)
+        job = body.get("job")
+        with self.make_session() as session:
+            job = Job(uuid=job_uuid, **job)
+            session.add(job)
+        with self.make_session() as session:
+            job = session.query(Job).filter_by(uuid = job_uuid).first()
+
+            self.set_header("Access-Control-Allow-Origin", "*")
+            self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type")
+            self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+            self.write({"Job": self.get_job(job)})
+    
+    @coroutine
+    def options(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, Content-Type")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.write("")
+
+        
 
 class AsyncWebRequestHandler(RequestHandler, SessionMixin):
     @coroutine
@@ -129,11 +213,13 @@ class AsyncWebRequestHandler(RequestHandler, SessionMixin):
 
 
 if __name__ == '__main__':
+    options.database_url = 'sqlite+pysqlite:///sqlite.db'
     session_factory = make_session_factory(options.database_url)
     DeclarativeBase.metadata.create_all(session_factory.engine)
 
     Application([
         (r'/', IndexHandler),
+        (r'/jobs', JobHandler),
         (r'/user', UserHandler),
         (r'/user/reg', UserRegHandler),
         (r'/user/login', UserLoginHandler),
